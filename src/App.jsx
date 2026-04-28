904eed74155f7a1e22a0c2b3627513733ea06181
TRALEDON <yuldashevasror759@gmail.com>
2026-04-28 15:38:46 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css"
+const products = [
+  { name: "Mars rug", price: 100, days: "24 dona mavjud" },
+  { name: "Keyboard sticker", price: 49, days: "10 dona mavjud" },
+  { name: "Smart watch", price: 899, days: "4 dona mavjud" },
+  { name: "Wireless keyboard", price: 350, days: "9 dona mavjud" },
+  { name: "Mouse", price: 359, days: "24 dona mavjud" },
+  { name: "AirPods", price: 499, days: "11 dona mavjud" },
+  { name: "Powerbank", price: 899, days: "4 dona mavjud" },
+  { name: "USB flash drive", price: 299, days: "21 dona mavjud" },
+  { name: "Smartphone", price: 3699, days: "2 dona mavjud" },
+  { name: "PlayStation 5", price: 7499, days: "1 dona mavjud" },
+  { name: "Yandex Station", price: 1999, days: "4 dona mavjud" },
+  { name: "Samsung Planet", price: 4499, days: "24 dona mavjud" },
+];
+
+function App() {
+  return (
+    <div className="list"></div>
+  );
+}
+
+export default App;
